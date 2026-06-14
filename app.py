8850b723ded0b58967b37f702d8a4378e573c285
KIRUTHIKA L <22bm022@drngpit.ac.in>
2026-06-14 18:35:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,20 @@
 import streamlit as st
-import pandas as pd
-import joblib
-
-# Load model
-model = joblib.load("ckd_model (4).pkl")
 
 st.title("Chronic Kidney Disease Prediction")
 
-age = st.number_input("Age", min_value=1, max_value=100, value=30)
+age = st.number_input("Age")
 
-bp = st.number_input("Blood Pressure (mm/Hg)", min_value=50, max_value=200, value=120)
+bp = st.number_input("Blood Pressure")
 
-creatinine = st.number_input("Serum Creatinine (mg/dl)", min_value=0.1, max_value=20.0, value=1.0)
+creatinine = st.number_input("Serum Creatinine")
 
-urea = st.number_input("Blood Urea (mg/dl)", min_value=1, max_value=400, value=25)
+urea = st.number_input("Blood Urea")
 
-egfr = st.number_input("eGFR", min_value=1, max_value=150, value=90)
+egfr = st.number_input("eGFR")
 
 if st.button("Predict"):
 
-    input_data = pd.DataFrame([[
-        age,
-        bp,
-        creatinine,
-        urea,
-        egfr
-    ]], columns=[
-        'Age of the patient',
-        'Blood pressure (mm/Hg)',
-        'Serum creatinine (mg/dl)',
-        'Blood urea (mg/dl)',
-        'Estimated Glomerular Filtration Rate (eGFR)'
-    ])
-
-    prediction = model.predict(input_data)[0]
-
-   if creatinine > 1.5 or urea > 50 or egfr < 60:
-    st.error("CKD Risk Detected")
-else:
-    st.success("No CKD Risk Detected")
+    if creatinine > 1.5 or urea > 50 or egfr < 60:
+        st.error("CKD Risk Detected")
+    else:
+        st.success("No CKD Risk Detected")
